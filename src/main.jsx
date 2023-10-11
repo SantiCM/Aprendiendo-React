4aba33587b9226d315be0a09f99ae34194f5be9b
Santiago Melo <santiagocano15cr7@gmail.com>
2023-10-10 19:47:31 -0600

    useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,12 @@ import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
 import { MemoHook } from './06-memo/MemoHook'
+import { CallbackHook } from './06-memo/CallbackHook'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
+      <CallbackHook></CallbackHook>
       <MemoHook></MemoHook>
       <Layout></Layout>
       <HooksApp></HooksApp>
