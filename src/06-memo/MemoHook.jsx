1f141ae77823a5a7b7e9369b05ad13783e26f5c4
Santiago Melo <santiagocano15cr7@gmail.com>
2023-10-10 19:34:27 -0600

    memo hook

--- src/06-memo/MemoHook.jsx
+++ src/06-memo/MemoHook.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react"
 import { useCounter } from "../Hooks"
+import { useMemo } from "react"
+
+
+const heavyStuff = (iterationNumber = 100) => {
+  for(let i = 0; i < iterationNumber; i++) {
+    console.log(heavyStuff);
+  }
+
+  return `${iterationNumber} iteraciones realizadas`
+}
 
 
 export const MemoHook = () => {
 
-    const {counter, increment} = useCounter(10)
+  const {counter, increment} = useCounter(4000)
 
-    const [show, setShow] = useState(true)
+  const [show, setShow] = useState(true)
+
+  const messageMemorize = useMemo(() => heavyStuff(counter), [counter])
 
   return (
     <>
         <h1>Counter: <small>{counter}</small></h1>
+
+        <h4>{messageMemorize}</h4>
 
         <button 
             className="btn btn-primary"
--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,12 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { MemoHook } from './06-memo/MemoHook'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
+      <MemoHook></MemoHook>
       <Layout></Layout>
       <HooksApp></HooksApp>
       <FocusScreen></FocusScreen>
